ec64d016aa42855b1254d6ac3d35373165eabf27
BV344 <123995573+BV344@users.noreply.github.com>
2023-12-05 11:37:51 -0800

    Added some code at the end of main.cpp
    1:
    Converting 1 Pixel Red
    2:
    Prints out the File Information

--- main.cpp
+++ main.cpp
@@ -2,44 +2,56 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-    if( argc != 3 )
-    {
-        cout << "Usage: ColorBMPtoGrayscale <input_filename> <output_filename>"
-             << endl << endl;
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: ColorBMPtoGrayscale <input_filename> <output_filename>" << endl << endl;
         return 1;
     }
 
     // Declare and read the bitmap
     BMP Input;
-    Input.ReadFromFile( argv[1]);
+    Input.ReadFromFile(argv[1]);
 
     // Convert each pixel to grayscale using RGB->YUV
-    for(int j = 0; j < Input.TellHeight(); j++) {
-        for(int i = 0; i < Input.TellWidth(); i++) {
+    for (int j = 0; j < Input.TellHeight(); j++) {
+        for (int i = 0; i < Input.TellWidth(); i++) {
             // Convert the RGB values of the current pixel to grayscale
-            int Temp = (int) floor(0.299 * Input(i, j)->Red +
-                                   0.587 * Input(i, j)->Green +
-                                   0.114 * Input(i, j)->Blue);
+            int Temp = (int)floor(0.299 * Input(i, j)->Red +
+                                  0.587 * Input(i, j)->Green +
+                                  0.114 * Input(i, j)->Blue);
 
             // Convert the grayscale value to a byte
-            ebmpBYTE TempBYTE = (ebmpBYTE) Temp;
+            ebmpBYTE TempBYTE = (ebmpBYTE)Temp;
 
             // Update the RGB values of the current pixel with the grayscale value
-            Input(i, j)->Red   = TempBYTE;
+            Input(i, j)->Red = TempBYTE;
             Input(i, j)->Green = TempBYTE;
-            Input(i, j)->Blue  = TempBYTE;
+            Input(i, j)->Blue = TempBYTE;
         }
     }
-
+    //This is for testing purposes and Find out the Coordinate System
+    /*
+    // Make one specific pixel red
+    int redPixelX = 639;
+    int redPixelY = 425;
+    Input(redPixelX, redPixelY)->Red = 255;
+    Input(redPixelX, redPixelY)->Green = 0;
+    Input(redPixelX, redPixelY)->Blue = 0;
+    */
     // Create a grayscale color table if necessary
-    if(Input.TellBitDepth() < 16){
-        CreateGrayscaleColorTable( Input);
+    if (Input.TellBitDepth() < 16) {
+        CreateGrayscaleColorTable(Input);
     }
 
+//    This tells you the Input File Info:
+//    cout << "File info:" << endl;
+//    cout << Input.TellWidth() << " x " << Input.TellHeight()
+//         << " at " << Input.TellBitDepth() << " bpp" << endl;
+
+
+
     // Write the output file
-    Input.WriteToFile( argv[2] );
+    Input.WriteToFile(argv[2]);
 
     return 0;
 }
